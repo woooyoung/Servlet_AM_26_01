adda0b5e0637200bb68dd3319e7ca31a08aaa828
woooyoung <axdsw121@gmail.com>
2026-01-14 10:30:26 +0900

    서블릿 구구단 기초

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -0,0 +1,33 @@
+package com.KoreaIT.java.AM_jsp;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/printDan")
+public class PrintDanServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		response.getWriter().append("==8단==<br>");
+
+//		response.getWriter().append("8 * 1 = 8<br>");
+//		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 1, 8));
+//		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 2, 16));
+
+		int dan = 8;
+
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+
+	}
+
+}
