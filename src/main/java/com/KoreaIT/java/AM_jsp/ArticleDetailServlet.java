79339ad3f7f593e753a7e7b42f96f934ea8e6f05
woooyoung <axdsw121@gmail.com>
2026-01-15 09:45:15 +0900

    상세페이지 구현, a 태그 활용

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/ArticleDetailServlet.java
@@ -0,0 +1,67 @@
+package com.KoreaIT.java.AM_jsp;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/detail")
+public class ArticleDetailServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		System.out.println(123);
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 없음");
+			e.printStackTrace();
+		}
+
+		String url = "jdbc:mysql://127.0.0.1:3306/Servlet_AM_26_01?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			response.getWriter().append("연결 성공");
+
+			DBUtil dbUtil = new DBUtil(request, response);
+
+			int id = Integer.parseInt(request.getParameter("id"));
+
+			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
+
+			Map<String, Object> articleRow = dbUtil.selectRow(conn, sql);
+
+			request.setAttribute("articleRow", articleRow);
+
+			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+			System.out.println("에러 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
