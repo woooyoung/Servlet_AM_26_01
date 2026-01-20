cb99004686dd8e0b6bbe4d10decbc1f69796be31
woooyoung <axdsw121@gmail.com>
2026-01-20 10:37:11 +0900

    DAO 구조 추가, List에만 적용

--- src/main/java/com/KoreaIT/java/AM_jsp/service/ArticleService.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/service/ArticleService.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
 
+import com.KoreaIT.java.AM_jsp.dao.ArticleDao;
 import com.KoreaIT.java.AM_jsp.util.DBUtil;
 import com.KoreaIT.java.AM_jsp.util.SecSql;
 
@@ -11,27 +12,21 @@ public class ArticleService {
 
 	private Connection conn;
 
+	private ArticleDao articleDao;
+
 	public ArticleService(Connection conn) {
 		this.conn = conn;
+		this.articleDao = new ArticleDao(conn);
 	}
 
 	public int getTotalCnt() {
-		SecSql sql = SecSql.from("SELECT COUNT(*)");
-		sql.append("FROM article;");
 
-		return DBUtil.selectRowIntValue(conn, sql);
+		return articleDao.getTotalCnt();
 	}
 
 	public List<Map<String, Object>> getForPrintArticles(int limitFrom, int itemsInAPage) {
 
-		SecSql sql = SecSql.from("SELECT A.*, M.name");
-		sql.append("FROM article AS A");
-		sql.append("INNER JOIN `member` AS M");
-		sql.append("ON A.memberId = M.id");
-		sql.append("ORDER BY A.id DESC");
-		sql.append("LIMIT ?, ?;", limitFrom, itemsInAPage);
-
-		return DBUtil.selectRows(conn, sql);
+		return articleDao.getForPrintArticles(limitFrom, itemsInAPage);
 	}
 
 }
